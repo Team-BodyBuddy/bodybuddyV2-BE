bbc3428e4b475ad66bba713b82217c5a4c5d0791
Dongjun <ldj990517@gmail.com>
2025-03-11 00:24:51 +0900

    Intial commit

---
+++ src/main/java/com/example/bodybuddy/BodybuddyApplication.java
@@ -0,0 +1,13 @@
+package com.example.bodybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodybuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodybuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bodybuddy/BodybuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bodybuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodybuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
